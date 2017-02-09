a1e99a63d13e22e4e9bcdc3da6a8eba4f2f41cd7
Adrian Smykowski <fisk.smk@gmail.com>
2017-02-09 23:44:25 +0100

    deprecated's removed

--- src/main/java/io/github/fisksmk/AmazonPolllySimpleJavaClient.java
+++ src/main/java/io/github/fisksmk/AmazonPolllySimpleJavaClient.java
@@ -1,8 +1,8 @@
 package io.github.fisksmk;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.polly.AmazonPollyClient;
+import com.amazonaws.auth.*;
+import com.amazonaws.services.polly.AmazonPolly;
+import com.amazonaws.services.polly.AmazonPollyClientBuilder;
 import com.amazonaws.services.polly.model.OutputFormat;
 import com.amazonaws.services.polly.model.SynthesizeSpeechRequest;
 import com.amazonaws.services.polly.model.SynthesizeSpeechResult;
@@ -17,20 +17,20 @@ public class AmazonPolllySimpleJavaClient {
 
     private static final String accessKey = "";
     private static final String secretKey = "";
-    private static final String endpoint = "polly.eu-west-1.amazonaws.com";
-
-    public static AWSCredentials awsCredentials;
+    private static final String region = "eu-west-1";
 
     public static void synthesize(String voiceName, String text, String filepath) throws IOException {
-        awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
 
         SynthesizeSpeechRequest synthesizeSpeechRequest = new SynthesizeSpeechRequest();
         synthesizeSpeechRequest.setOutputFormat(OutputFormat.Mp3);
         synthesizeSpeechRequest.setVoiceId(voiceName);
         synthesizeSpeechRequest.setText(text);
 
-        AmazonPollyClient client = new AmazonPollyClient(awsCredentials);
-        client.setEndpoint(endpoint);
+        AmazonPollyClientBuilder clientBuilder = AmazonPollyClientBuilder.standard();
+        clientBuilder.setCredentials(new AWSStaticCredentialsProvider(awsCredentials));
+        clientBuilder.setRegion(region);
+        AmazonPolly client = clientBuilder.build();
         SynthesizeSpeechResult synthesizeSpeechResult = client.synthesizeSpeech(synthesizeSpeechRequest);
         InputStream audioStream = synthesizeSpeechResult.getAudioStream();
 
